8ea80cb7dd01e4a562bd66c431e11b54d1ea535c
Giovanni Agenor Marchi <giovanni.marchi@compasso.com.br>
2023-03-15 18:14:38 -0300

    feat: teste

---
+++ src/main/java/br/com/gmarchiprojects/bomerpsystem/BomErpSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.gmarchiprojects.bomerpsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BomErpSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BomErpSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gmarchiprojects/bomerpsystem/BomErpSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gmarchiprojects.bomerpsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BomErpSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
